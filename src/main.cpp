2bc0a30a2c804a057548390160630d540a1f4bc0
Gunter <gw6sk@arcor.de>
2022-12-03 08:53:45 +0100

    Built mit 767x lib erfolgreich

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino_OV767X.h>
+#include <wire.h>
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
